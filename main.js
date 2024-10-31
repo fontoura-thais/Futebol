a6d4a3ac9d3df853f697828880c6808ccb175780
fontoura-thais <147612390+fontoura-thais@users.noreply.github.com>
2024-10-31 12:06:29 -0300

    q

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 alert("Futebolas")
 function criarCartao(pergunta,resposta){
    let conteudo = document.getElementById("conteudo")
-console.log(conteudo)
+let cartao = document.createElement('article')
+cartao.classList = 'cartao'
+
+
+cartao.innerHTML = `<div class="cartao--conteudo">
+        <div class="cartao--pergunta"><p>Quem foi o campeão da copa paulista de 2021 ? </p></div>
+         <div class="cartao--resposta"><p>O campeonato teve como campeão o São Paulo</p></div>
+        </div>`
+
+conteudo.appendChild(cartao)
+
 }
 criarCartao(1,2)
