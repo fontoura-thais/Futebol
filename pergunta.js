ef3f6099717551349f6949bf5df316e42facc0e4
fontoura-thais <147612390+fontoura-thais@users.noreply.github.com>
2024-11-01 08:43:00 -0300

    b

--- pergunta.js
+++ pergunta.js
@@ -10,3 +10,15 @@ criarCartao(
     "Clube que mais revelou jogadores?",
     "São Paulo é quem mais revela jogadores no País"
 )
+criarCartao( 
+    "Quem foi o campeão da copa paulista de 2021?",
+    "O campeonato teve como campeão o São Paulo"
+)
+criarCartao( 
+    "Quem foi o primeiro campeão brasileiro?",
+    "O Cruzeiro foi o primeiro campeão nesse formato."
+)
+criarCartao( 
+    "Clube com mais títulos nacionais?",
+    "Flamengo e Grêmio"
+)
