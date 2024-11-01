8e44f6ff4d2d2a9184980324248f577abbefcf7c
fontoura-thais <147612390+fontoura-thais@users.noreply.github.com>
2024-11-01 08:33:45 -0300

    x

---
+++ pergunta.js
@@ -0,0 +1,12 @@
+criarCartao( 
+    "Maior artilheiro do corinthians de 2024?",
+    "Yuri Alberto: 12 gols em 24 jogos."
+)
+criarCartao( 
+    "Jogador mais caro do futebol brasileiro de 2024",
+    "Thiago Almada (Botafogo) - R$ 137,4 milhões em 2024."
+)
+criarCartao( 
+    "Clube que mais revelou jogadores?",
+    "São Paulo é quem mais revela jogadores no País"
+)
